27e0eb607e2b3b009a8ce68c4239423aeb772b1e
SrijhaM <srijha04@gmail.com>
2025-02-12 11:59:22 +0530

    Add files via upload

---
+++ short circuit r&l.java
@@ -0,0 +1,13 @@
+class Main
+{
+   public static void main (String s[])
+   {
+      int a=20,b=30,c=5;
+      if(a>=b&&a==c)
+      System.out.println(a+"is the greatest");
+      else if(b>=a&&b>=c)
+      System.out.println(b+"is the greatest");
+      else
+      System.out.println(c+"is the greatest");
+   }
+}
