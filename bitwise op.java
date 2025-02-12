e92875ea506c6f51130384adce6cc8ac053f6d63
SrijhaM <srijha04@gmail.com>
2025-02-12 12:49:00 +0530

    Add files via upload

---
+++ bitwise op.java
@@ -0,0 +1,13 @@
+class Main
+{
+   public static void main (String s[])
+   {
+     int a=492,b=333;
+     int c=a&b;
+     System.out.println("a&b =" + c);
+     int d=a|b;
+     System.out.println("a|b =" + d);
+     int e=a^b;
+     System.out.println("a^b =" + e);
+   }
+}
