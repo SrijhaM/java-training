75df6f06bff0579456305f1c2ef509a4ef9be9b9
SrijhaM <srijha04@gmail.com>
2025-02-18 10:23:58 +0530

    Add files via upload
    pascal triangle pattern

---
+++ NumPattern.java
@@ -0,0 +1,21 @@
+class NumPattern
+{
+    public static void main(String[] args)
+    {
+        int rows = 5;        
+        for (int i = 0; i < rows; i++)
+        {
+            int n = 1;
+            for (int j = 0; j < rows - i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print(n + " ");
+                n = n * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
