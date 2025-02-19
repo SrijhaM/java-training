4e47197bbcc1c9ffe5005cda6c4b24767e0bee86
SrijhaM <srijha04@gmail.com>
2025-02-19 09:54:32 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class string
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in); 
+        System.out.print("Enter the String:");
+        int a =in.nextInt();
+        System.out.println("String:" +a);
+    }
+}
