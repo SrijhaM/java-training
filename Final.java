5e33f3acb04d23a57fd340453ef676a92d550ac0
SrijhaM <srijha04@gmail.com>
2025-02-24 15:35:47 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,45 @@
+abstract class AbstractClass
+{
+    private static final String CONSTANT_VALUE = "This is a constant";
+    private final int value;
+
+    public AbstractClass(int value)
+    {
+        this.value = value;
+    }
+
+    public abstract void displayInfo();
+
+    public static String getConstantValue()
+    {
+        return CONSTANT_VALUE;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+}
+
+class ConcreteClass extends AbstractClass
+{
+    public ConcreteClass(int value)
+    {
+        super(value);
+    }
+
+    public void displayInfo()
+    {
+        System.out.println("Value: " + getValue());
+        System.out.println("Constant: " + getConstantValue());
+    }
+}
+
+public class Final
+{
+    public static void main(String[] args) {
+        ConcreteClass obj = new ConcreteClass(10);
+        obj.displayInfo();
+    }
+}
+
