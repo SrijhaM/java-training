2a6327950d1d10e1d84e9dd82179012bc0040099
SrijhaM <srijha04@gmail.com>
2025-03-21 15:20:42 +0530

    Add files via upload

---
+++ BinaryAdd.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class BinaryAdd
+{
+    public static String addBinary(String a, String b) 
+    {
+     StringBuilder result = new StringBuilder();
+     int i = a.length()-1;
+     int j = b.length()-1;
+     int carry=0;
+     while(i>=0 || j>=0)
+     {
+     int sum = carry;
+     if(i>=0)sum+=a.charAt(i--)-'0';
+     if(j>=0)sum+=b.charAt(j--)-'0';
+     result.append(sum%2);
+     carry = sum/2;
+     }
+     if(carry!=0)result.append(carry);
+     return result.reverse().toString();
+    }
+    public static void main(String args[])
+    {
+    String a = "11";
+    String b = "1";
+    BinaryAdd obj = new BinaryAdd();
+    String result = obj.addBinary(a, b);
+    System.out.println("Sum of the binary strings: " + result);
+    }
+}
