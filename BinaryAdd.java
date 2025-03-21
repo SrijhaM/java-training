156e1d35104e0dc84d4a0aae42225882963e78fb
SrijhaM <srijha04@gmail.com>
2025-03-21 15:38:31 +0530

    BinaryAdd.java

--- BinaryAdd.java
+++ BinaryAdd.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 class BinaryAdd
 {
-    public static String addBinary(String a, String b) 
+    public String addBinary(String a, String b) 
     {
-     StringBuilder result = new StringBuilder();
-     int i = a.length()-1;
-     int j = b.length()-1;
-     int carry=0;
-     while(i>=0 || j>=0)
-     {
-     int sum = carry;
-     if(i>=0)sum+=a.charAt(i--)-'0';
-     if(j>=0)sum+=b.charAt(j--)-'0';
-     result.append(sum%2);
-     carry = sum/2;
-     }
-     if(carry!=0)result.append(carry);
-     return result.reverse().toString();
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) 
+        {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) result.append(carry);
+        return result.reverse().toString();
     }
-    public static void main(String args[])
+    public static void main(String[] args) 
     {
-    String a = "11";
-    String b = "1";
-    BinaryAdd obj = new BinaryAdd();
-    String result = obj.addBinary(a, b);
-    System.out.println("Sum of the binary strings: " + result);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the first binary string: ");
+        String a = in.nextLine();
+        System.out.print("Enter the second binary string: ");
+        String b = in.nextLine();
+        BinaryAdd obj = new BinaryAdd();
+        String result = obj.addBinary(a, b);
+        System.out.println("Sum of the binary strings: " + result);
     }
 }
