3a9e088f29e713a9ca1ead25419d72bfb0979b2a
SrijhaM <srijha04@gmail.com>
2025-03-24 12:12:53 +0530

    Diamondpattern.java

--- diamondpattern.java
+++ diamondpattern.java
@@ -1,29 +1,36 @@
-class Main {
+class dimondpattern
+	{
 	public static void main(String[] args)
 	{
-		int number = 4;
+		int num = 4;
 		int m = 1;
 		int n;
-		while (m <= number) {
+		while (m <= num) 
+		{
 			n = 1;
-			while (n++ <= number - m) {
+			while (n++ <= num - m) 
+			{
 				System.out.print(" ");
 			}
 			n = 1;
-			while (n++ <= m * 2 - 1) {
+			while (n++ <= m * 2 - 1)
+				{
 				System.out.print("*");
 			}
 			System.out.println();
 			m++;
 		}
-		m = number - 1;
-		while (m > 0) {
+		m = num - 1;
+		while (m > 0)
+			{
 			n = 1;
-			while (n++ <= number - m) {
+			while (n++ <= num - m)
+				{
 				System.out.print(" ");
 			}
 			n = 1;
-			while (n++ <= m * 2 - 1) {
+			while (n++ <= m * 2 - 1) 
+			{
 				System.out.print("*");
 			}
 			System.out.println();
