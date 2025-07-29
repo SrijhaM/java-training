72e3c2e2b1a96b272036f62f6611d2db083eedab
SrijhaM <srijha04@gmail.com>
2025-07-29 10:48:58 +0530

    Add files via upload

---
+++ AddNumberStrings.java
@@ -0,0 +1,13 @@
+public class AddNumberStrings 
+{
+    public static void main(String[] args) 
+    {
+        String num1 = "124";
+        String num2 = "456";
+        int n1 = Integer.parseInt(num1);
+        int n2 = Integer.parseInt(num2);
+        int sum = n1 + n2;
+        System.out.println("Sum: " + sum);
+    }
+}
+ 
